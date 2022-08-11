70f54073a673f792e107a85f48758b54dac88cbc
kellykang-tech <kellykang13@gmail.com>
2022-08-11 16:11:45 +0900

    feat(coordinate): x,y 좌표값이 24를 넘으면 예외가 발생하는 기능 구현

--- src/main/java/coordinate/Coordinate.java
+++ src/main/java/coordinate/Coordinate.java
@@ -4,12 +4,24 @@ import java.util.Objects;
 
 public class Coordinate {
 
+    private final int MAX_NUMBER = 24;
     private final int x;
     private final int y;
 
     public Coordinate(int x, int y) {
+        validateCoordinateValue(x, y);
         this.x = x;
         this.y = y;
+    }
+
+    private void validateCoordinateValue(int x, int y) {
+        if (isOverMaxNumber(x, y)) {
+            throw new IllegalArgumentException("x, y의 값은 24를 초과하면 안됩니다.");
+        }
+    }
+
+    private boolean isOverMaxNumber(int x, int y) {
+        return x > MAX_NUMBER || y > MAX_NUMBER;
     }
 
     public boolean isMatched(Coordinate coordinate) {
--- src/test/java/coordinate/CoordinateTest.java
+++ src/test/java/coordinate/CoordinateTest.java
@@ -1,5 +1,6 @@
 package coordinate;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,10 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CoordinateTest {
 
     @Test
-    void create() {
-        Coordinate coordinate = new Coordinate(10, 5);
+    @DisplayName("x,y의 좌표값이 24를 넘으면 IllegalArgumentException 예외가 발생한다.")
+    void 좌표값이_24를_넘는_경우() {
+        assertThrows(IllegalArgumentException.class, () -> new Coordinate(25, 25));
+    }
 
-        assertTrue(coordinate.isMatched(new Coordinate(10, 5)));
+    @Test
+    void create() {
+        Coordinate coordinate = new Coordinate(24, 24);
+
+        assertTrue(coordinate.isMatched(new Coordinate(24, 24)));
         assertFalse(coordinate.isMatched(new Coordinate(5, 5)));
     }
 }
