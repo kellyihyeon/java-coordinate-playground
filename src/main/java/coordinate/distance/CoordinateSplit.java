fd6a08691023e6c9dba15ae2e6400b9cde6932dc
kellykang-tech <kellykang13@gmail.com>
2022-08-11 23:40:03 +0900

    refactor(coordinateSplit): byComma 메소드 반환값: list ->  Coordinate 객체를 반환한다

--- src/main/java/coordinate/distance/CoordinateSplit.java
+++ src/main/java/coordinate/distance/CoordinateSplit.java
@@ -1,24 +1,20 @@
 package coordinate.distance;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class CoordinateSplit {
 
-    public static List<Integer> byComma(String input) {
+    public static Coordinate byComma(String input) {
         final int FIRST = 0;
         final int SECOND = 1;
         String stringX = input.split(",")[FIRST].replace("(", "");
         String stringY = input.split(",")[SECOND].replace(")", "");
 
-        return Arrays.asList(Integer.parseInt(stringX), Integer.parseInt(stringY));
+        return new Coordinate(Integer.parseInt(stringX), Integer.parseInt(stringY));
     }
 
     public static Coordinates byDash(String input) {
         Coordinates coordinates = new Coordinates();
         for (String rawCoordinate : input.split("-")) {
-            List<Integer> nums = byComma(rawCoordinate);
-            coordinates.add(new Coordinate(nums.get(0), nums.get(1)));
+            coordinates.add(byComma(rawCoordinate));
         }
         return coordinates;
     }
--- src/test/java/coordinate/CoordinateSplitTest.java
+++ src/test/java/coordinate/CoordinateSplitTest.java
@@ -4,7 +4,6 @@ import coordinate.distance.Coordinate;
 import coordinate.distance.CoordinateSplit;
 import coordinate.distance.Coordinates;
 import org.junit.jupiter.api.Test;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,9 +21,7 @@ public class CoordinateSplitTest {
     @Test
     void 좌표값이_하나일_때() {
         String input = "(10,10)";
-        List<Integer> coordinates = CoordinateSplit.byComma(input);
-
-        assertEquals(10, coordinates.get(0));
-        assertEquals(10, coordinates.get(1));
+        Coordinate coordinate = CoordinateSplit.byComma(input);
+        assertEquals(new Coordinate(10, 10), coordinate);
     }
 }
